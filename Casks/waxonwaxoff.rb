d2dd45979f54546501f466f78aa7e83ade32f04e
sevmorris <15384535+sevmorris@users.noreply.github.com>
2026-08-19 16:56:20 -0700

    Bump waxonwaxoff to 2.8.0

--- Casks/waxonwaxoff.rb
+++ Casks/waxonwaxoff.rb
@@ -1,6 +1,6 @@
 cask "waxonwaxoff" do
-  version "2.7.0"
-  sha256 "df2e8a93151e61f7c090b5d8f3fdbd3284c4095231223c3711cddee1b43af112"
+  version "2.8.0"
+  sha256 "a385ced93b61d1b6bdbb602bbb7971e2fcaac7e71521a7826ba8498f8b4c4c8d"
 
   url "https://github.com/sevmorris/WaxOnWaxOff/releases/download/v#{version}/WaxOnWaxOff-v#{version}.dmg",
       verified: "github.com/sevmorris/WaxOnWaxOff/"
